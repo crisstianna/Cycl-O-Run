0dd3d49efac70f64509f5742986453935152acbe
KevInvicto <kevinvicto139@gmail.com>
2020-05-25 16:25:45 +0200

    Home in progress

--- cyclorun/content/themes/cyclorun/app/js/app.js
+++ cyclorun/content/themes/cyclorun/app/js/app.js
@@ -1,7 +1,14 @@
 var app = {
   init: function() {
     console.log('init');
-  }
+
+    const signInButtonElement= document.querySelector('connexion-button');
+   console.log(signInButtonElement);
+
+  },
+  handleSignIn: function (evt){
+    console.log('I want sign in')
+  } 
 };
 
 $(app.init);
