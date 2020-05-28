d3c56f9514ff4138eb92d24d60b422e7cae525ef
KevInvicto <59162686+KevInvicto@users.noreply.github.com>
2020-05-28 17:14:46 +0200

    Revert "Personalised home"

--- cyclorun/content/themes/cyclorun/app/js/app.js
+++ cyclorun/content/themes/cyclorun/app/js/app.js
@@ -1,14 +1,7 @@
 var app = {
   init: function() {
     console.log('init');
-
-    const signInButtonElement= document.querySelector('connexion-button');
-   console.log(signInButtonElement);
-
-  },
-  handleSignIn: function (evt){
-    console.log('I want sign in')
-  } 
+  }
 };
 
 $(app.init);
