1a98671577b5c6c8c67f111678d5b750a1495f86
KevInvicto <kevinvicto139@gmail.com>
2020-05-28 18:39:06 +0200

    Manual Recovery of celine branch

--- cyclorun/content/themes/cyclorun/custom_home.php
+++ cyclorun/content/themes/cyclorun/custom_home.php
@@ -8,6 +8,7 @@
 
 if(is_user_logged_in()){
     wp_loginout(home_url());
+    
 } else {
     header('Location: http://localhost/Projets/projet-cycl-o-run/cyclorun/login/');
 }
@@ -16,3 +17,56 @@ echo 'Je suis la Home Personalisé';
 
 
 ?>
+
+</article>        
+      </div>
+    </section>
+    <!-- Participation section -->
+    <section class="outing">
+      <h2 class="outing__title"><em>Prochainement ...... </em>Les sorties auxquelles je participe</h2>
+      <div class="outing__section">
+        <article class="outing__article">
+          <div class="outing__article__image">
+            <img class="outing__article__img" src="images/logo-o.png" alt="">
+          </div>
+          <div>
+            <h3 class="outing__article__title">Nom de la sortie</h3>
+            <p class="outing__article__date">date</p>
+            <p class="outing__article__location">Lieu</p>
+            <p class="outing__article__distance">Distance</p>
+            <p class="outing__article__level">intensif</p>
+            <button class="outing__article__button" type="button">Etat de la sortie</button>
+          </div>
+        </article>
+        <article class="outing__article">
+          <div class="outing__article__image">
+            <img class="outing__article__img" src="images/logo-o.png" alt="">
+          </div>
+          <div>
+            <h3 class="outing__article__title">Nom de la sortie</h3>
+            <p class="outing__article__date">date</p>
+            <p class="outing__article__location">Lieu</p>
+            <p class="outing__article__distance">Distance</p>
+            <p class="outing__article__level">intensif</p>
+            <button class="outing__article__button" type="button">Etat de la sortie</button>
+          </div>
+        </article>
+        <article class="outing__article">
+          <div class="outing__article__image">
+            <img class="outing__article__img" src="images/logo-o.png" alt="">
+          </div>
+          <div>
+            <h3 class="outing__article__title">Nom de la sortie</h3>
+            <p class="outing__article__date">date</p>
+            <p class="outing__article__location">Lieu</p>
+            <p class="outing__article__distance">Distance</p>
+            <p class="outing__article__level">intensif</p>
+            <button class="outing__article__button" type="button">Etat de la sortie</button>
+          </div>
+        </article>
+      </div>
+    </section>
+  </main>
+  <script src="js/app.js"></script>
+</body>
+</html>
---
+++ cyclorun/content/themes/cyclorun/outing_registration.php
@@ -0,0 +1,72 @@
+<?php
+
+require 'template-parts/outing-creation-form.php';
+
+//var_dump($_POST);
+$id = get_current_user_id();
+
+// TODO récupérer l'id de l'auteur et ajouter le textarea dans la table
+
+global $wpdb;
+
+$outingName = filter_input(INPUT_POST, 'outing_name');
+$address = filter_input(INPUT_POST, 'address');
+$date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_NUMBER_FLOAT);
+$time = filter_input(INPUT_POST, 'time');
+$distance = filter_input(INPUT_POST, 'distance', FILTER_SANITIZE_NUMBER_INT);
+$practicedSport = filter_input(INPUT_POST, 'practicedSport');
+$level = filter_input(INPUT_POST, 'level');
+$picture = filter_input(INPUT_POST, 'picture', FILTER_SANITIZE_URL);
+$description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_SPECIAL_CHARS);
+
+//var_dump($description);
+
+$errors = [];
+
+
+if (empty($outingName)) {
+    $errors['outing_name'] = 'Veuillez entrer un nom pour votre sortie';
+}
+if (empty($address)) {
+    $errors['address'] = 'Veuillez entrer une adresse pour votre sortie';
+}    
+if (empty($date)) {
+    $errors['date'] = 'Veuillez renseigner la date de votre sortie';
+}
+if (empty($time)) {
+    $errors['time'] = 'Veuillez saisir l\'heure de rendez-vous pour votre sortie';
+}
+if (empty($distance)) {
+    $errors['distance'] = 'Veuillez entrer la distance prévue pour le parcours';
+}   
+if (empty($practicedSport)) {
+    $errors['practiced_sport'] = 'Veuillez choisir le type de sport pour la sortie';
+} 
+if (empty($level)) {
+    $errors['level'] = 'Veuillez sélectionner le niveau de votre sortie';
+}
+if (empty($picture)) {
+    // TODO $picture = 'A déterminer';
+}
+if (empty($description)) {
+    $errors['course'] = 'Veuillez donner une description du parcours';
+}
+else {
+    $wpdb->insert(
+        $wpdb->prefix . 'outings',
+        array(
+            'outing_name' => $outingName,
+            'author' => $id,
+            'address' => $address,                
+            'date' => $date,
+            'time' => $time,
+            'distance' => $distance,
+            'practiced_sport' => $practicedSport,
+            'level' => $level,
+            'picture' => $picture,
+            'description' => $description
+        )
+    );
+    // header("Location: ");
+    // exit;
+}
