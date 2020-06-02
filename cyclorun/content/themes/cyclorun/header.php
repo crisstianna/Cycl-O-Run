ef0c92d9e56c6bc8474d4629ccae076a0e0c919b
Augendre <celine.augendre@hotmail.com>
2020-06-02 20:05:16 +0200

    first step for dynamic  participation-page

--- cyclorun/content/themes/cyclorun/header.php
+++ cyclorun/content/themes/cyclorun/header.php
@@ -11,7 +11,7 @@
     <nav class="navbar authentified navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="<?php echo get_permalink(7); ?>"><img class="logo" src=" <?= get_stylesheet_directory_uri() .'/public/images/logo-complet.png';?>" alt=""></a>
       <a class="navbar__item__is-active" href="<?php echo get_permalink(7); ?>">Home</a>
-      <a class="navbar__item" href="#">Profil</a>
+      <a class="navbar__item" href="<?php echo get_permalink(72); ?>">Profil</a>
       <a class="navbar__item" href="<?php echo get_permalink(40); ?>">Participer à une sortie</a>
       <a class="navbar__item" href="<?php echo get_permalink(16); ?>">Organiser une sortie</a>
       <?php if(is_user_logged_in()): ?>
