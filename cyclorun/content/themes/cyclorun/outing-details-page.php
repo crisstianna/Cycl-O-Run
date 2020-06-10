c9ddebdf3f5d016368713c8d540e938c25dd2ba1
KevInvicto <kevinvicto139@gmail.com>
2020-06-10 08:30:40 +0200

    update app.js

--- cyclorun/content/themes/cyclorun/outing-details-page.php
+++ cyclorun/content/themes/cyclorun/outing-details-page.php
@@ -187,34 +187,10 @@ if (!empty($_POST)) {
     }
 }
 
+
+get_footer();
+
 ?>
 
     
 
-<script>
-  let latitude = Number(document.getElementById('map').dataset.lat);
-      console.log(latitude);
-  let longitude = Number(document.getElementById('map').dataset.lgt);
-      console.log(longitude);
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat:latitude, lng:longitude},
-          zoom: 17,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          scrollwheel: false,
-        });
-        var marker = new google.maps.Marker({
-      // Nous définissons sa position (syntaxe json)
-      position: {lat: latitude, lng: longitude},
-      // Nous définissons à quelle carte il est ajouté
-      map: map
-});
-      }
-      window.onload = function(){
-				// Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
-				initMap(); 
-			};
-    </script>
-    <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBmSOdXll4S7xEjhUhMzhOk7wCIarU30Ek&callback=initMap"
-    async defer></script> -->
